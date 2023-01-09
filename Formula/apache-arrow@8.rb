5b70383910202022a205c206941d4a9874be968a
Ricky McMillen <ricky.mcmillen@gmail.com>
2023-01-09 16:44:24 +1300

    Initial commit and Apache Arrow 8 tap

---
+++ Formula/apache-arrow@8.rb
@@ -0,0 +1,96 @@
+class ApacheArrowAT8 < Formula
+  desc "Columnar in-memory analytics layer designed to accelerate big data"
+  homepage "https://arrow.apache.org/"
+  url "https://www.apache.org/dyn/closer.lua?path=arrow/arrow-8.0.0/apache-arrow-8.0.0.tar.gz"
+  mirror "https://archive.apache.org/dist/arrow/arrow-8.0.0/apache-arrow-8.0.0.tar.gz"
+  sha256 "ad9a05705117c989c116bae9ac70492fe015050e1b80fb0e38fde4b5d863aaa3"
+  license "Apache-2.0"
+  revision 4
+  head "https://github.com/apache/arrow.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "4125c0769a1f3fb51c0f252d82ab290e63a1d16061399ee5be20b31626617767"
+    sha256 cellar: :any,                 arm64_big_sur:  "17a820a99038829114389477546eece3980a0b98d65a52738bb88ffb894d5b8d"
+    sha256 cellar: :any,                 monterey:       "c48250a2db3f527a8eb34d52d613e7147d41feccb2224fcef945497264818ed3"
+    sha256 cellar: :any,                 big_sur:        "68933d8e138a7bb31f50f9c81f6c57bc29a14d088e0dc36f8d1e1f6d74fa42d9"
+    sha256 cellar: :any,                 catalina:       "0353aa904e1021bf745f400c77e52074ec2303d3224cb908364bb00a8511005b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9afa6875e381a717a6aedaef7542808c23af433be338dffcbe7f63e5a3af8ef8"
+  end
+
+  depends_on "boost" => :build
+  depends_on "cmake" => :build
+  depends_on "llvm" => :build
+  depends_on "aws-sdk-cpp"
+  depends_on "brotli"
+  depends_on "glog"
+  depends_on "grpc"
+  depends_on "lz4"
+  depends_on "numpy"
+  depends_on "openssl@1.1"
+  depends_on "protobuf"
+  depends_on "python@3.10"
+  depends_on "rapidjson"
+  depends_on "re2"
+  depends_on "snappy"
+  depends_on "thrift"
+  depends_on "utf8proc"
+  depends_on "zstd"
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
+  def install
+    # https://github.com/Homebrew/homebrew-core/issues/76537
+    ENV.runtime_cpu_detection if Hardware::CPU.intel?
+
+    # https://github.com/Homebrew/homebrew-core/issues/94724
+    # https://issues.apache.org/jira/browse/ARROW-15664
+    ENV["HOMEBREW_OPTIMIZATION_LEVEL"] = "O2"
+
+    # link against system libc++ instead of llvm provided libc++
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
+    args = %W[
+      -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=TRUE
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+      -DARROW_FLIGHT=ON
+      -DARROW_GANDIVA=ON
+      -DARROW_JEMALLOC=ON
+      -DARROW_ORC=ON
+      -DARROW_PARQUET=ON
+      -DARROW_PLASMA=ON
+      -DARROW_PROTOBUF_USE_SHARED=ON
+      -DARROW_PYTHON=ON
+      -DARROW_S3=ON
+      -DARROW_WITH_BZ2=ON
+      -DARROW_WITH_ZLIB=ON
+      -DARROW_WITH_ZSTD=ON
+      -DARROW_WITH_LZ4=ON
+      -DARROW_WITH_SNAPPY=ON
+      -DARROW_WITH_BROTLI=ON
+      -DARROW_WITH_UTF8PROC=ON
+      -DARROW_INSTALL_NAME_RPATH=OFF
+      -DPython3_EXECUTABLE=#{which("python3")}
+    ]
+
+    args << "-DARROW_MIMALLOC=ON" unless Hardware::CPU.arm?
+
+    system "cmake", "-S", "cpp", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "arrow/api.h"
+      int main(void) {
+        arrow::int64();
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++11", "-I#{include}", "-L#{lib}", "-larrow", "-o", "test"
+    system "./test"
+  end
+end
