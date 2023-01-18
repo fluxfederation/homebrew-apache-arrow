e4973b9c034bce03b9647b28ba105bab7a0d9f8d
Jeremy Olliver <jeremy.olliver@fluxfederation.com>
2023-01-18 14:56:45 +1300

    Restore monterey support declaration, and compare changes between arrow 8 -> 10

--- Formula/apache-arrow@8.rb
+++ Formula/apache-arrow@8.rb
@@ -1,6 +1,7 @@
 class ApacheArrowAT8 < Formula
   desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
+  # TODO: https://archive.apache.org/dist/arrow/arrow-8.0.1/apache-arrow-8.0.1.tar.gz
   url "https://www.apache.org/dyn/closer.lua?path=arrow/arrow-8.0.0/apache-arrow-8.0.0.tar.gz"
   mirror "https://archive.apache.org/dist/arrow/arrow-8.0.0/apache-arrow-8.0.0.tar.gz"
   sha256 "ad9a05705117c989c116bae9ac70492fe015050e1b80fb0e38fde4b5d863aaa3"
@@ -10,7 +11,9 @@ class ApacheArrowAT8 < Formula
 
   bottle do
     sha256 cellar: :any,                 arm64_ventura:  "4125c0769a1f3fb51c0f252d82ab290e63a1d16061399ee5be20b31626617767"
+    sha256 cellar: :any,                 arm64_monterey: "4125c0769a1f3fb51c0f252d82ab290e63a1d16061399ee5be20b31626617767"
     sha256 cellar: :any,                 ventura:        "c48250a2db3f527a8eb34d52d613e7147d41feccb2224fcef945497264818ed3"
+    sha256 cellar: :any,                 monterey:       "c48250a2db3f527a8eb34d52d613e7147d41feccb2224fcef945497264818ed3"
   end
 
   depends_on "boost" => :build
@@ -18,20 +21,23 @@ class ApacheArrowAT8 < Formula
   depends_on "llvm" => :build
   depends_on "aws-sdk-cpp"
   depends_on "brotli"
+  # depends_on "bzip2" # From master formula (arrow@10.0.1)
   depends_on "glog"
   depends_on "grpc"
   depends_on "lz4"
-  depends_on "numpy"
+  depends_on "numpy" # Removed in master formula (arrow@10.0.1)
   depends_on "openssl@1.1"
   depends_on "protobuf"
-  depends_on "python@3.10"
+  depends_on "python@3.10" # Removed in master formula (arrow@10.0.1)
   depends_on "rapidjson"
   depends_on "re2"
   depends_on "snappy"
   depends_on "thrift"
   depends_on "utf8proc"
+  # depends_on "z3" # From master formula (arrow@10.0.1)
   depends_on "zstd"
 
+  # on_linux not present in arrow@10.0.1 although that does declare linux support
   on_linux do
     depends_on "gcc"
   end
@@ -39,11 +45,8 @@ class ApacheArrowAT8 < Formula
   fails_with gcc: "5"
 
   def install
-    # ==========================================================================
-    # TODO figure out how to ignore!!!
-    # ==========================================================================
     # https://github.com/Homebrew/homebrew-core/issues/76537
-    # ENV.runtime_cpu_detection if Hardware::CPU.intel?
+    ENV.runtime_cpu_detection if Hardware::CPU.intel?
 
     # https://github.com/Homebrew/homebrew-core/issues/94724
     # https://issues.apache.org/jira/browse/ARROW-15664
